cde79cbeb544ba124eb8e51caa9a639386aa5dbb
Papon khan <paponk25@gmail.com>
2026-07-01 23:01:28 +0600

    lucd-tost-farmarmotion add

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/MainRouter";
 import "./index.css";
@@ -7,5 +8,12 @@ import "./index.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <Toaster
+      position="top-center"
+      reverseOrder={false}
+      toastOptions={{
+        duration: 4000,
+      }}
+    />
   </StrictMode>,
 );
